131b3b40638cd02661a970e5285564b880bf1dfe
Hungtran1905 <hungtran112002@gmail.com>
2025-04-29 21:40:17 +0900

    import css

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,11 @@
+import "./style.css"
+
 const MyComponent = () => {
     return (
-      <div>hoidanit</div>
+      // do file .jsx chỉ có thể chứa 1 phần tử html nên sẽ dùng fragment <></> để chứa các phần tử html còn lại thì nó sẽ không có lỗi xảy ra
+      <> 
+        <div className="hello">Hello World</div>
+      </>
     );
   }
 

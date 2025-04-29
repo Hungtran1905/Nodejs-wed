1c8df7ab4ff9162ce892b93e685cd51889d5b513
Hungtran1905 <hungtran112002@gmail.com>
2025-04-29 21:54:35 +0900

    cách sử dụng biến số với JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,14 @@
 import "./style.css"
 
 const MyComponent = () => {
+  const age = 24
     return (
       // do file .jsx chỉ có thể chứa 1 phần tử html nên sẽ dùng fragment <></> để chứa các phần tử html còn lại thì nó sẽ không có lỗi xảy ra
       <> 
         <div className="hello">Hello World</div>
+        <div style={{color:"green"}}>Hello React</div>
+        <div>I am {age}</div>
+        <div>{console.log("xin chao")}</div>
       </>
     );
   }

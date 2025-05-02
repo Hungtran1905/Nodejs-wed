fb69ddea71f4c6ffc2b9e98bb56f432610ce6d11
Hungtran1905 <hungtran112002@gmail.com>
2025-05-02 17:27:40 +0900

    xử lý lỗi Not Found

--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,13 @@ import UsersPage from './pages/user.jsx'
 import ProductPage from './pages/product.jsx'
 import "./styles/global.css"
 import TodoApp from './components/todo/TodoApp.jsx'
+import ErrorPage from './pages/error.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,

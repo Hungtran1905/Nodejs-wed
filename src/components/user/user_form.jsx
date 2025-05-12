332e9c974c10e164516a63bee45cacc75c8da251
Hungtran1905 <hungtran112002@gmail.com>
2025-05-12 22:55:41 +0900

    sử lý lỗi với Interceptor

--- src/components/user/user_form.jsx
+++ src/components/user/user_form.jsx
@@ -1,22 +1,27 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api_services";
+
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [phone, setPhoneNumber] = useState("");
-    const handleClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone);
+        if (res.data) {
+            notification.success({
+                message: "create user",
+                description: "Tạo user thành công!"
+            })
+        } else {
+            notification.error({
+                message: "Error create user",
+                description: JSON.stringify(res.message)
+            })
         }
-        axios.post(URL_BACKEND, data)
-        return (console.log({ fullName, email, password, phone }))
+
     }
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>

dcd5404b219b61309ae91274851140183473b2f9
Hungtran1905 <hungtran112002@gmail.com>
2025-08-04 15:44:28 +0900

    chia layout

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Form, notification } from "antd";
+import { Button, Input, Form, notification, Row, Col } from "antd";
 import { registerUserAPI } from "../services/api_services";
 import { useNavigate } from "react-router-dom";
 
@@ -30,51 +30,68 @@ const RegisterPage = () => {
             form={form}
             layout="vertical"
             onFinish={onFinish}
-
+            style={{ margin: "10px" }}
         // onFinish={onFinish}
         // onFinishFailed={onFinishFailed}
         >
-            <div style={{ margin: "50px" }}>
 
-                <Form.Item
-                    label="Full Name"
-                    name="fullName"
-                    rules={[{ required: true, message: 'Please input your full name!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[{ required: true, message: 'Please input your email!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
-                <Form.Item
-                    label="Phone Number"
-                    name="phone"
-                    rules={[{
-                        required: true,
-                        pattern: new RegExp(/\d+/g),
-                        message: "Wrong format!"
-                    }]}
-                >
-                    <Input />
-                </Form.Item>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Full Name"
+                        name="fullName"
+                        rules={[{ required: true, message: 'Please input your full name!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: 'Please input your email!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
+                    <Form.Item
+                        label="Phone Number"
+                        name="phone"
+                        rules={[{
+                            required: true,
+                            pattern: new RegExp(/\d+/g),
+                            message: "Wrong format!"
+                        }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Col>
+            </Row>
 
-            </div>
+            <Row justify={"center"}>
+                <Col xs={24} md={8}>
 
-            <div style={{ margin: "50px" }}>
+                    <Button onClick={() => form.submit()} type="primary">Register</Button>
+                </Col>
+            </Row>
 
-                <div><Button onClick={() => form.submit()} type="primary">Register</Button></div>
-            </div>
+
         </Form >
 
 

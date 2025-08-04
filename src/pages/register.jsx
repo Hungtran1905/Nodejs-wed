79a0686e2328bc40b184d950497c0b41cfdd9f49
Hungtran1905 <hungtran112002@gmail.com>
2025-08-04 13:25:12 +0900

    sử dụng uncontrol

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,60 @@
+import { Button, Input, Form } from "antd";
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => {
+        console.log('Success:', values);
+    }
     return (
-        <div>register page</div>
+
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+
+        // onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        >
+            <div style={{ margin: "50px" }}>
+
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                // rules={[{ required: true, message: 'Please input your full name!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                // rules={[{ required: true, message: 'Please input your email!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                // rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone Number"
+                    name="phone"
+                // rules={[{ required: true, message: 'Please input your phone number!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+            </div>
+
+            <div style={{ margin: "50px" }}>
+
+                <div><Button onClick={() => form.submit()} type="primary">Register</Button></div>
+            </div>
+        </Form>
+
+
     )
 }
-
-export default RegisterPage
+export default RegisterPage;
